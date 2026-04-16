dd10524ee35b1212278a484cec853c2c6568b101
HeronNepomuceno <heronnepo1@gmail.com>
2026-04-16 10:06:51 -0300

    feat: register each page created on App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
+import DashboardPage from './pages/dashboard'
 import OrdersPage from './pages/orders'
+import TecnicosPage from './pages/tecnicos'
 
 function App() {
   return (
     <Routes>
-      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/" element={<DashboardPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/ordens-de-servico" element={<OrdersPage />} />
+      <Route path="/tecnicos" element={<TecnicosPage />} />
     </Routes>
   )
 }
